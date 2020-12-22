c943ad593fa4a4d80bf9781573469e735cfffaf7
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2020-12-22 21:41:42 +0100

    Format todos on result

--- src/ElmTestRunner/Reporter/ConsoleBis.elm
+++ src/ElmTestRunner/Reporter/ConsoleBis.elm
@@ -9,7 +9,6 @@ import ElmTestRunner.Vendor.ConsoleFormat exposing (format)
 import ElmTestRunner.Vendor.ConsoleText as Text exposing (..)
 import ElmTestRunner.Vendor.FormatColor as FormatColor
 import ElmTestRunner.Vendor.FormatMonochrome as FormatMonochrome
-import Json.Encode as Encode exposing (Value)
 import Test.Runner exposing (formatLabels)
 
 
@@ -29,6 +28,7 @@ onBegin { seed, fuzzRuns } testsCount =
     """
 Running {{ testsCount }} tests. To reproduce these results later, run:
 elm-test-rs --seed {{ seed }} --fuzz {{ fuzzRuns }} {{ files }}
+
 """
         |> String.replace "{{ testsCount }}" (String.fromInt testsCount)
         |> String.replace "{{ seed }}" (String.fromInt seed)
@@ -37,39 +37,77 @@ elm-test-rs --seed {{ seed }} --fuzz {{ fuzzRuns }} {{ files }}
         |> Just
 
 
+
+--
+
+
 onResult : TestResult -> Maybe String
-onResult result =
-    case result of
+onResult testResult =
+    let
+        useColor =
+            -- Text.Monochrome
+            Text.UseColor
+    in
+    case testResult of
         Passed _ ->
             Nothing
 
         Failed { labels, todos, failures, logs } ->
-            Just <|
-                String.join "\n"
-                    [ ""
-                    , formatLabels labels
-                    , ""
-                    , indent (displayFailureContent todos failures logs)
-                    ]
+            if List.isEmpty todos then
+                -- We have non-TODOs still failing; report them, not the TODOs.
+                failuresToText useColor labels failures
+                    |> Text.render useColor
+                    |> Just
+
+            else
+                List.map (\todo -> formatTodo labels todo ++ "\n") todos
+                    |> String.concat
+                    |> Just
 
 
-formatLabels : List String -> String
-formatLabels =
-    formatLabelsHelp []
+formatTodo : List String -> String -> String
+formatTodo labels todo =
+    String.concat
+        [ "◦ "
+        , String.concat (List.map (\l -> l ++ " → ") labels)
+        , "TODO: "
+        , todo
+        ]
 
 
-formatLabelsHelp : List String -> List String -> String
-formatLabelsHelp formattedLines labels =
-    case ( formattedLines, labels ) of
-        ( _, [] ) ->
-            String.join "\n" formattedLines
+failuresToText : UseColor -> List String -> List Failure -> Text
+failuresToText useColor labels failures =
+    Text.concat (failureLabelsToText labels :: List.map (failureToText useColor) failures)
 
-        -- First is the test name
-        ( [], testName :: location ) ->
-            formatLabelsHelp [ "X " ++ testName ] location
 
-        ( _, loc :: location ) ->
-            formatLabelsHelp (("| " ++ loc) :: formattedLines) location
+failureLabelsToText : List String -> Text
+failureLabelsToText =
+    formatLabels (dark << plain << withChar '↓') (red << withChar '✗') >> Text.concat
+
+
+failureToText : UseColor -> Failure -> Text
+failureToText useColor { given, description, reason } =
+    let
+        formatEquality =
+            case useColor of
+                Monochrome ->
+                    FormatMonochrome.formatEquality
+
+                UseColor ->
+                    FormatColor.formatEquality
+
+        messageText =
+            plain ("\n" ++ indent (format formatEquality description reason) ++ "\n\n")
+    in
+    case given of
+        Nothing ->
+            messageText
+
+        Just givenStr ->
+            [ dark (plain ("\nGiven " ++ givenStr ++ "\n"))
+            , messageText
+            ]
+                |> Text.concat
 
 
 indent : String -> String
@@ -79,84 +117,8 @@ indent str =
         |> String.join "\n"
 
 
-displayFailureContent : List String -> List Failure -> List String -> String
-displayFailureContent todos failures logs =
-    "with todos: {{ todos }}\nwith failures: {{ failures }}\nwith debug logs:\n\n{{ logs }}\n"
-        |> String.replace "{{ todos }}" (Debug.toString todos)
-        |> String.replace "{{ failures }}" (Debug.toString failures)
-        |> String.replace "{{ logs }}" (String.concat logs)
 
-
-
--- reportComplete : UseColor -> TestResult -> Value
--- reportComplete useColor testResult =
---     case testResult of
---         TestResult.Passed _ ->
---             -- No failures of any kind.
---             Encode.null
 --
---         TestResult.Failed { labels, failures, todos } ->
---             if List.isEmpty todos then
---                 -- We have non-TODOs still failing; report them, not the TODOs.
---                 failures
---                     |> failuresToText useColor labels
---                     |> textToValue useColor
---
---             else
---                 Encode.object
---                     [ ( "todo", Encode.string (String.concat todos) )
---                     , ( "labels", Encode.list Encode.string labels )
---                     ]
---
---
--- textToValue : UseColor -> Text -> Value
--- textToValue useColor txt =
---     txt
---         |> Text.render useColor
---         |> Encode.string
---
---
--- failuresToText : UseColor -> List String -> List Failure -> Text
--- failuresToText useColor labels failures =
---     Text.concat (failureLabelsToText labels :: List.map (failureToText useColor) failures)
---
---
--- failureLabelsToText : List String -> Text
--- failureLabelsToText =
---     formatLabels (dark << plain << withChar '↓') (red << withChar '✗') >> Text.concat
---
---
--- failureToText : UseColor -> Failure -> Text
--- failureToText useColor { given, description, reason } =
---     let
---         formatEquality =
---             case useColor of
---                 Monochrome ->
---                     FormatMonochrome.formatEquality
---
---                 UseColor ->
---                     FormatColor.formatEquality
---
---         messageText =
---             plain ("\n" ++ indent (format formatEquality description reason) ++ "\n\n")
---     in
---     case given of
---         Nothing ->
---             messageText
---
---         Just givenStr ->
---             [ dark (plain ("\nGiven " ++ givenStr ++ "\n"))
---             , messageText
---             ]
---                 |> Text.concat
---
---
--- indent : String -> String
--- indent str =
---     str
---         |> String.split "\n"
---         |> List.map ((++) "    ")
---         |> String.join "\n"
 
 
 onEnd : Result String Kind -> Array TestResult -> Maybe String
@@ -174,7 +136,8 @@ formatSummary : Kind -> Summary -> String
 formatSummary kind { totalDuration, passedCount, failedCount, todoCount } =
     let
         useColor =
-            Text.Monochrome
+            -- Text.Monochrome
+            Text.UseColor
 
         headlineResult =
             case ( kind, failedCount, todoCount ) of
